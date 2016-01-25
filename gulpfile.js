cf44b99a6c719df697d36f887df2672609257c67
Amir Gafurov <gafurovamir@gmail.com>
2016-01-25 13:44:22 +0300

    adding Foundation SCSS -> SASS fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     prefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin'),
-    compass = require('gulp-compass'),
+    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     rigger = require('gulp-rigger'),
     imagemin = require('gulp-imagemin'),
@@ -14,24 +14,24 @@ var gulp = require('gulp'),
     opn = require('opn');
 
 var path = {
-    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+    build: {
         html: 'build/',
         js: 'build/js/',
         css: 'build/css/',
         img: 'build/images/',
         fonts: 'build/fonts/'
     },
-    src: { //Пути откуда брать исходники
-        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
-        style: 'src/sass/main.scss',
-        img: 'src/images/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/sass/main.sass',
+        img: 'src/images/**/*.*',
         fonts: 'src/fonts/**/*.*'
     },
-    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+    watch: {
         html: 'src/**/*.html',
         js: 'src/js/**/*.js',
-        style: 'src/sass/**/*.scss',
+        style: 'src/sass/**/*.*',
         img: 'src/images/**/*.*',
         fonts: 'src/fonts/**/*.*'
     },
@@ -44,51 +44,55 @@ var server = {
 };
 
 gulp.task('html:build', function () {
-    gulp.src(path.src.html) //Выберем файлы по нужному пути
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
-        .pipe(connect.reload()); //И перезагрузим наш сервер для обновлений
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(connect.reload());
 });
 
 gulp.task('js:build', function () {
-    gulp.src(path.src.js) //Найдем наш main файл
-        .pipe(rigger()) //Прогоним через rigger
-        //.pipe(sourcemaps.init()) //Инициализируем sourcemap
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(sourcemaps.write()) //Пропишем карты
-        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
-        .pipe(connect.reload()); //И перезагрузим сервер
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        //.pipe(sourcemaps.init())
+        .pipe(uglify())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(connect.reload());
 });
 
 gulp.task('style:build', function () {
-    gulp.src(path.src.style) //Выберем наш main.scss
-        .pipe(sourcemaps.init()) //То же самое что и с js
-        .pipe(compass({
-            css: 'src/css',
-            sass: 'src/sass',
-            image: 'src/images',
-            font: 'src/fonts'
-        })) //Скомпилируем
-        .pipe(prefixer()) //Добавим вендорные префиксы
-        .pipe(cssmin()) //Сожмем
+    gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        /*.pipe(compass({
+         css: 'src/css',
+         sass: 'src/sass',
+         image: 'src/images',
+         font: 'src/fonts'
+         }))*/
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(prefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssmin())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(gulp.dest(path.build.css))
         .pipe(connect.reload());
 });
 
 gulp.task('image:build', function () {
-    gulp.src(path.src.img) //Выберем наши картинки
-        .pipe(imagemin({ //Сожмем их
+    gulp.src(path.src.img)
+        .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(gulp.dest(path.build.img))
         .pipe(connect.reload());
 });
 
-gulp.task('fonts:build', function() {
+gulp.task('fonts:build', function () {
     gulp.src(path.src.fonts)
         .pipe(gulp.dest(path.build.fonts))
 });
@@ -101,25 +105,25 @@ gulp.task('build', [
     'image:build'
 ]);
 
-gulp.task('watch', function(){
-    watch([path.watch.html], function(event, cb) {
+gulp.task('watch', function () {
+    watch([path.watch.html], function (event, cb) {
         gulp.start('html:build');
     });
-    watch([path.watch.style], function(event, cb) {
+    watch([path.watch.style], function (event, cb) {
         gulp.start('style:build');
     });
-    watch([path.watch.js], function(event, cb) {
+    watch([path.watch.js], function (event, cb) {
         gulp.start('js:build');
     });
-    watch([path.watch.img], function(event, cb) {
+    watch([path.watch.img], function (event, cb) {
         gulp.start('image:build');
     });
-    watch([path.watch.fonts], function(event, cb) {
+    watch([path.watch.fonts], function (event, cb) {
         gulp.start('fonts:build');
     });
 });
 
-gulp.task('webserver', function() {
+gulp.task('webserver', function () {
     connect.server({
         host: server.host,
         port: server.port,
@@ -131,8 +135,8 @@ gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
 });
 
-gulp.task('openbrowser', function() {
-    opn( 'http://' + server.host + ':' + server.port + '/build' );
+gulp.task('openbrowser', function () {
+    opn('http://' + server.host + ':' + server.port + '/build');
 });
 
 gulp.task('default', ['build', 'webserver', 'watch']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/modernizr/modernizr.js
+//= ../../bower_components/foundation/js/foundation.min.js
+
+$(document).foundation();
 
 //= partials/app.js
