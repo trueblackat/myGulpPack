0b180cc214052a873a9336a30ea886dc5d8da0a7
Amir Gafurov <gafurovamir@gmail.com>
2016-02-03 12:26:35 +0300

    upgrade Foundation from 5 to 6 version, adding folders and files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 //= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/modernizr/modernizr.js
-//= ../../bower_components/foundation/js/foundation.min.js
+// ../../bower_components/modernizr/modernizr.js
+//= ../../bower_components/foundation-sites/dist/foundation.min.js
 
 $(document).foundation();
 
